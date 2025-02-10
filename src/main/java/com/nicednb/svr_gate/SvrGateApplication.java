e54233f934155ee783a0c1f1e2d4620181c110b5
hellolcs <hellolcs84@gmail.com>
2025-02-10 16:50:52 +0900

    최초 업로드

---
+++ src/main/java/com/nicednb/svr_gate/SvrGateApplication.java
@@ -0,0 +1,13 @@
+package com.nicednb.svr_gate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvrGateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvrGateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicednb/svr_gate/SvrGateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicednb.svr_gate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvrGateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
