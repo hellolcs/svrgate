f64603ee4a74f278a5487e5e1eaa79b8c10564ed
hellolcs <hellolcs84@gmail.com>
2025-03-17 16:11:37 +0900

    PasswordChangeInterceptor에서 NonNull 어노테이션을 Spring의 것으로 변경

--- src/main/java/com/nicednb/svrgate/config/PasswordChangeInterceptor.java
+++ src/main/java/com/nicednb/svrgate/config/PasswordChangeInterceptor.java
@@ -4,10 +4,11 @@ import com.nicednb.svrgate.entity.Account;
 import com.nicednb.svrgate.service.SystemSettingService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.lang.NonNull;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
