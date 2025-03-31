2a3bc79c1632117659247e48cd3c0efc0cf017e9
hellolcs <hellolcs84@gmail.com>
2025-03-31 17:13:22 +0900

    RestTemplate 구성 개선: HttpClient에 대한 커넥션 풀 설정 추가 및 SSLContext 설정 변경

--- src/main/java/com/nicednb/svrgate/config/RestTemplateConfig.java
+++ src/main/java/com/nicednb/svrgate/config/RestTemplateConfig.java
@@ -3,51 +3,46 @@ package com.nicednb.svrgate.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
 import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
-import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
-import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactoryBuilder;
 import org.apache.hc.client5.http.ssl.TrustAllStrategy;
 import org.apache.hc.core5.ssl.SSLContexts;
+import org.apache.hc.core5.util.Timeout;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 
 import javax.net.ssl.SSLContext;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class RestTemplateConfig {
 
     @Bean
-    public RestTemplate restTemplate() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
-        // SSL 인증서 검증 우회 설정 (개발 환경에서만 사용)
+    public RestTemplate restTemplate() throws Exception {
+        // SSLContext 설정 (모든 인증서를 신뢰)
         SSLContext sslContext = SSLContexts.custom()
                 .loadTrustMaterial(null, new TrustAllStrategy())
                 .build();
-        
-        SSLConnectionSocketFactory sslSocketFactory = SSLConnectionSocketFactoryBuilder.create()
-                .setSslContext(sslContext)
-                .setHostnameVerifier(new NoopHostnameVerifier())
-                .build();
-        
-        PoolingHttpClientConnectionManager connectionManager = 
-            new PoolingHttpClientConnectionManager();
+
+        // 커넥션 풀 설정
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
         connectionManager.setMaxTotal(100);
         connectionManager.setDefaultMaxPerRoute(20);
 
+        // HttpClient 생성
         CloseableHttpClient httpClient = HttpClients.custom()
                 .setConnectionManager(connectionManager)
-                .setSSLSocketFactory(sslSocketFactory) // 이 메서드가 올바르게 적용됨
+                .setSSLContext(sslContext) // SSLContext 설정
+                .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE) // 호스트네임 검증 비활성화
+                .evictExpiredConnections()
                 .build();
 
-        HttpComponentsClientHttpRequestFactory factory = 
-            new HttpComponentsClientHttpRequestFactory(httpClient);
-        factory.setConnectTimeout(3000); // 3초
-        factory.setReadTimeout(5000);    // 5초
+        // RestTemplate에 HttpClient 설정
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+        requestFactory.setConnectTimeout(3000); // 연결 타임아웃 3초
+        requestFactory.setReadTimeout(5000); // 읽기 타임아웃 5초
 
-        return new RestTemplate(factory);
+        return new RestTemplate(requestFactory);
     }
 }
