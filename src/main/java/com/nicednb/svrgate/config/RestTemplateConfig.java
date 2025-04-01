1e92a3a3bacbd5613657846e4e388fc6672f6baa
hellolcs <hellolcs84@gmail.com>
2025-04-01 13:21:31 +0900

    RestTemplate 구성 개선: HttpClientBuilder 임포트 제거

--- src/main/java/com/nicednb/svrgate/config/RestTemplateConfig.java
+++ src/main/java/com/nicednb/svrgate/config/RestTemplateConfig.java
@@ -4,7 +4,6 @@ import java.util.concurrent.TimeUnit;
 
 import org.apache.hc.client5.http.config.RequestConfig;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
 import org.apache.hc.core5.util.Timeout;
